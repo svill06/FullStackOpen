b5b0f1a28a49fa14dc8f946534a95cd94c270b8d
svill06 <167185933+svill06@users.noreply.github.com>
2025-08-28 10:13:57 +0300

    Update Part.jsx

--- Osa 2/kurssitiedot/src/Components/Part.jsx
+++ Osa 2/kurssitiedot/src/Components/Part.jsx
@@ -1,10 +1,7 @@
-const Part = ({ part }) => {
-    return (
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    )
-  }
-  
-  export default Part
-  
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+export default Part
