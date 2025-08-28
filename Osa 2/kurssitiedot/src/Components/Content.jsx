8561acf7f724551dadb6913293dd595b604bb481
svill06 <167185933+svill06@users.noreply.github.com>
2025-08-28 10:14:41 +0300

    Update Content.jsx

--- Osa 2/kurssitiedot/src/Components/Content.jsx
+++ Osa 2/kurssitiedot/src/Components/Content.jsx
@@ -1,13 +1,11 @@
 import Part from './Part'
 
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  )
-}
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
 
 export default Content
